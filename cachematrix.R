30f55d37f7ca20ba047b1eafcec7a9e9a3bb787e
omaksymov <oleksandr.maksymov@gmail.com>
2014-06-23 01:24:59 +0300

    main commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function is for creating an object, 
+## which can contain inversed matrix. 
+## Created object is used in the second function.
 
-## Write a short comment describing this function
+## Here we create object to use for retrieving cached inversed matrix for input {x}.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inversed) inv <<- inversed
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Here we try to retrieve inversed matrix if it exists for object {x}, 
+## created using upper function. If it does - we just use already calculated value.
+## Else we calculate inversed matrix using {solve} function 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matr <- x$get()
+  inv <- solve(matr, ...)
+  x$setinv(inv)
+  inv
 }
